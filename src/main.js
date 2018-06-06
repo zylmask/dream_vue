bec7ba341de93fd9e615ccc8f7c6f92e88971af3
zylmask <zeptojs@163.com>
2018-06-06 13:58:17 +0800

    添加文件

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import elementUi from 'element-ui'
+import Vuex from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(elementUi)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/views/DreamHome.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "DreamHome"
+  }
+</script>
+
+<style scoped>
+
+</style>
