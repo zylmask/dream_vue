de0fe26e5b23447e6646a42498fa6a2961e872b1
zylmask <zeptojs@163.com>
2018-06-06 11:51:46 +0800

    修改配置项

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import elementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(elementUi)
 
 /* eslint-disable no-new */
 new Vue({
